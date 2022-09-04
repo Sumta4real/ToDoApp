d4b990957d017f4113e010f90176f108e3ae02f4
Sumayyah Taiwo <sumty4deen@gmail.com>
2022-09-05 00:55:56 +0900

    Added a test method to check the strength of the secret key

--- ToDoApp/tasks/tests.py
+++ ToDoApp/tasks/tests.py
@@ -1,3 +1,16 @@
+import os
 from django.test import TestCase
+from django.contrib.auth.password_validation import validate_password
 
 # Create your tests here.
+class TryDjangoConfigTest(TestCase):
+    #create method to test the strength of the secret key
+    def test_secret_key_srength(self):
+        SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+        try:
+            is_strong = validate_password(SECRET_KEY)
+        except Exception as e:
+            msg = f'Weak Secret Key {e.messages}'
+            self.fail(msg)
+        #self.assertTrue(1==1)
+
